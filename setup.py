d4dbaff125a9d2c00c25c466fa61c37ed4335656
Sergey <safilimonov@gmail.com>
2024-04-07 22:08:23 -0600

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="openparse",
-    version="0.4.1",
+    version="0.5.0",
     entry_points={
         "console_scripts": [
             "openparse-download= openparse.cli:download_unitable_weights",
